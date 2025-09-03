012c3a44b69ad571df3f1dc54a8d831776b8c97b
ezarwiratama <ezar.hardin10@gmail.com>
2025-09-03 22:13:11 +0700

    navbar, hero section, about section, still error in lanyard

---
+++ app/section/about.tsx
@@ -0,0 +1,72 @@
+import ScrollFloat from "@/components/ScrollFloat/ScrollFloat";
+import AnimatedContent from "@/components/AnimatedContent/AnimatedContent";
+// import Lanyard from "@/components/Lanyard/Lanyard";
+import BlurText from "@/components/BlurText/BlurText";
+import ShinyText from "@/components/ShinyText/ShinyText";
+
+export default function AboutSection() {
+  return (
+    <>
+        <div className="mt-20 mx-auto w-full max-w-[1600px] rounded-3xl" id="about">
+          <div className="text-center text-4xl font-bold mb-20">
+          <ScrollFloat
+            animationDuration={1}
+            ease="back.inOut(2)"
+            scrollStart="center bottom+=50%"
+            scrollEnd="bottom bottom-=40%"
+            stagger={0.03}
+          >About Me
+          </ScrollFloat>
+        </div>
+          <div className="flex flex-col md:flex-row items-center justify-between gap-10 pt-0">
+            <div className="basis-full md:basis-7/12 pr-0 md:pr-8 border-b md:border-b-0 md:border-r border-violet-500/30">
+              {/* Kolom kiri */}
+              <div className="flex-1 text-left">
+                <BlurText
+                  text="I'm Ezar Hardin Wiratama"
+                  delay={150}
+                  animateBy="words"
+                  direction="top"
+                  className="leading-relaxed text-3xl font-bold text-violet-400 mb-5"
+                />
+                <BlurText
+                  text="I am a Computer Engineering student at Diponegoro University 2023, and a passionate Fullstack Developer with around three years of programming experience. I have gained valuable professional exposure through one year of work experience and three internships. I am adaptable, capable of working well under pressure, and thrive both independently and as part of a team."
+                  delay={150}
+                  animateBy="words"
+                  direction="top"
+                  className="text-base md:text-lg leading-relaxed mb-10 text-gray-300"
+                />
+              </div>
+
+              <div className="flex flex-col sm:flex-row items-center sm:justify-between text-center sm:text-left gap-y-8 sm:gap-y-0 mb-4 w-full">
+                  <div>
+                    <h1 className="text-3xl md:text-4xl mb-1">
+                      4<span className="text-violet-500">+</span>
+                    </h1>
+                    <p>Project Finished</p>
+                  </div>
+                  <div>
+                    <h1 className="text-3xl md:text-4xl mb-1">
+                      3<span className="text-violet-500">+</span>
+                    </h1>
+                    <p>Years of Experience</p>
+                  </div>
+                  <div>
+                    <h1 className="text-3xl md:text-4xl mb-1">
+                      3.67<span className="text-violet-500">/4.00</span>
+                    </h1>
+                    <p>GPA</p>
+                  </div>
+              </div>
+            </div>
+
+            {/* Kolom kanan */}
+            <div className="basis-full md:basis-5/12 pl-0 md:pl-8 overflow-hidden max-w-full flex justify-center ">
+              {/* <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} /> */}
+            </div>
+          </div>
+
+        </div>
+    </>
+  );
+}
---
+++ app/section/hero.tsx
@@ -0,0 +1,95 @@
+import ProfileCard from "@/components/ProfileCard/ProfileCard";
+import ShinyText from "@/components/ShinyText/ShinyText";
+import TextType from "@/components/TextType/TextType";
+import BlurText from "@/components/BlurText/BlurText";
+
+export default function HeroSection() {
+  return (
+    <>
+      <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1 mb-12">
+        <div className="animate__animated animate__fadeInUp animate__delay-3s">
+          {/* quote section */}
+          <div className="flex items-center gap-3 mb-6 bg bg-zinc-800 w-fit py-3 px-4 rounded-2xl">
+            <img
+              src="/assets/ezar-2.png"
+              className="w-12 h-12 rounded-full border border-white/10 object-cover"
+            />
+            <q>Move forward or be left behind</q>
+          </div>
+          {/* typograph */}
+          <h1 className="text-5xl font-bold mb-6">
+            <ShinyText
+              text="Hi, I'm Ezar Hardin Wiratama"
+              disabled={false}
+              speed={3}
+              className="custom-class"
+            />
+          </h1>
+          {/* role */}
+          <h1 className="text-3xl font-semibold mb-6">
+            <TextType
+              text={[
+                "Fullstack Developer",
+                "Software Engineer",
+                "UI/UX Designer",
+              ]}
+              typingSpeed={75}
+              pauseDuration={1500}
+              showCursor={true}
+              cursorCharacter="|"
+            />
+          </h1>
+
+          <BlurText
+            text="A skilled and adaptable Full Stack Developer with expertise in building and maintaining web applications from front to back. I am passionate about creating efficient and user-friendly digital solutions."
+            delay={150}
+            animateBy="words"
+            direction="top"
+            className=" mb-6"
+          />
+
+          <div className="flex items-center sm:gap-4 gap-2">
+            <a
+              href="./assets/CV.pdf"
+              download="Faris_Edrik_Prayoga_CV.pdf"
+              className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
+            >
+              <ShinyText
+                text="Download CV"
+                disabled={false}
+                speed={3}
+                className="custom-class"
+              />
+            </a>
+
+            <a
+              href="#project"
+              className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
+            >
+              <ShinyText
+                text="Explore My Projects"
+                disabled={false}
+                speed={3}
+                className="custom-class"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="md:ml-auto animate__animated animate__fadeInUp animate__delay-4s">
+          <ProfileCard
+            name="Ezar Hardin W"
+            title="Fullstack Developer"
+            handle="ezarwiratama"
+            status="Online"
+            contactText="Contact Me"
+            avatarUrl="./assets/ezar-2.png"
+            showUserInfo={true}
+            enableTilt={true}
+            enableMobileTilt={false}
+            onContactClick={() => console.log("Contact clicked")}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
