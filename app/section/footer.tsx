17a47b7b81aae8f62886a791bcd7f35851be4c15
ezarwiratama <ezar.hardin10@gmail.com>
2025-09-09 10:54:08 +0700

    add footer

--- app/section/footer.tsx
+++ app/section/footer.tsx
@@ -1,8 +1,14 @@
+import ShinyText from "../Components/ShinyText/ShinyText";
 export default function Footer() {
   return (
     <footer className="bg-transparent border-t border-gray-700 text-gray-300 py-6 mt-10">
       <div className="max-w-6xl mx-auto px-4 text-center text-sm">
-        © {new Date().getFullYear()} Ezar Hardin. All rights reserved.
+        <ShinyText
+          text={`© ${new Date().getFullYear()} Ezar Hardin. All rights reserved.`}
+          disabled={false}
+          speed={3}
+          className="custom-class"
+        />
       </div>
     </footer>
   );
